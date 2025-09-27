9ccabb9dd946820b84e81a0a7797e6a87e07c4d9
Gürkan AYTEKİN <gurkanaytekin@yandex.com.tr>
2025-09-27 19:04:05 +0300

    configuration changed

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
 rootProject.name = "Yapp"
 include(":app")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
